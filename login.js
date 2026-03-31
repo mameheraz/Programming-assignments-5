981e9fca7d084dc9b485a305ecffcf2272ab07e0
mameheraz <mameheraz25@gmail.com>
2026-03-31 22:52:37 +0600

    Open Issues loaded and shown

--- login.js
+++ login.js
@@ -20,32 +20,4 @@ document.getElementById("login-btn").addEventListener("click", function () {
 })
 
 
-// Issues container started here
 
-async function loadIssues() {
-    const response = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
-    const issues = await response.json();
-
-    // get container
-    const container = document.getElementById("issues-container");
-    container.innerHTML = "";
-
-    issues.data.forEach(issue => {
-        const div = document.createElement("div");
-        div.innerHTML = `
-
-        <div class="border-t-4 border-[#00A96E] rounded-sm p-3 bg-white shadow-sm space-y-3 h-48">
-            <div class="flex justify-between items-center">
-                <img src="./assets/Open-Status.png" alt=""> 
-                <button class="btn bg-[#FEECEC] rounded-full h-5 font-medium text-[#EF4444] text-[12px]">${issue.priority}</button>
-            </div>
-
-            <h2 class="font-semibold text-[14px] text-[#1F2937]">${issue.title}</h2>
-            <p class="text-[#64748B] text-[12px] font-extralight">${issue.description}</p>
-        </div>
-        `;
-
-        container.appendChild(div);
-    })
-
-}
---
+++ mainpage.js
@@ -0,0 +1,153 @@
+// Active Button work now
+
+function activeButton(activeId) {
+    document.getElementById("all-btn").classList.remove("active-btn");
+    document.getElementById("open-btn").classList.remove("active-btn");
+    document.getElementById("close-btn").classList.remove("active-btn");
+
+    document.getElementById(activeId).classList.add("active-btn");
+}
+
+
+// Date convert to natural flow
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+}
+
+// -------------------------------------------------------------------------------------
+
+// Issues container started here
+
+async function loadIssues() {
+    const response = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    const issues = await response.json();
+
+    // get container
+    const container = document.getElementById("issues-container");
+    container.innerHTML = "";
+
+    issues.data.forEach(issue => {
+
+        const iconStatus = issue.status === "closed" ? "./assets/ClosedStatus.png" : "./assets/OpenStatus.png";
+        const borderColor = issue.status === "closed" ? "#A855F7"  : "#00A96E";
+
+        const div = document.createElement("div");
+        div.innerHTML = `
+
+        <div class="border-t-4 rounded-sm p-3 bg-white shadow-sm space-y-3 min-h-60" style="border-color:${borderColor}">
+            <div class="flex justify-between items-center">
+                <img src="${iconStatus}" alt=""> 
+                <button class="btn bg-[#FEECEC] rounded-full h-5 font-medium text-[#EF4444] text-[12px]">${issue.priority}</button>
+            </div>
+
+            <h2 class="font-semibold text-[14px] text-[#1F2937]">${issue.title}</h2>
+            <p class="text-[#64748B] text-[12px] font-extralight">${issue.description}</p>
+
+            
+
+            <hr class="text-gray-300">
+            <p class="text-[#64748B] text-[12px] font-[400]">#1by ${issue.author}</p>
+            <p class="text-[#64748B] text-[12px] font-[400]">${formatDate(issue.createdAt)}</p>
+        </div>
+        `;
+
+        container.appendChild(div);
+    });
+    issuesCount(issues.data.length);
+}
+
+
+// Open issues container
+
+// load data
+async function openIssues() {
+    const response = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const issues = await response.json();
+
+    // get the container 
+    const container = document.getElementById("issues-container");
+    container.innerHTML = "";
+
+    // Create loop , create div and append it
+
+    const open = issues.data.filter(issue => issue.status === "open")
+
+    open.forEach(issue => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="border-t-4 border-[#00A96E] rounded-sm p-3 bg-white shadow-sm space-y-3 min-h-60">
+            <div class="flex justify-between items-center">
+                <img src="./assets/OpenStatus.png" alt=""> 
+                <button class="btn bg-[#FEECEC] rounded-full h-5 font-medium text-[#EF4444] text-[12px]">${issue.priority}</button>
+            </div>
+
+            <h2 class="font-semibold text-[14px] text-[#1F2937]">${issue.title}</h2>
+            <p class="text-[#64748B] text-[12px] font-extralight">${issue.description}</p>
+            <hr class="text-gray-300">
+            <p class="text-[#64748B] text-[12px] font-[400]">#1by ${issue.author}</p>
+            <p class="text-[#64748B] text-[12px] font-[400]">${formatDate(issue.createdAt)}</p>
+        </div>
+        `;
+        container.appendChild(div);
+    });
+
+    issuesCount(open.length);
+}
+
+
+// closedd issues
+
+// load data
+async function ClosedIssues() {
+    const response = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const issues = await response.json();
+
+
+    // get the container
+    const container = document.getElementById("issues-container");
+    container.innerHTML = "";
+
+
+    // loop create div and append
+    const closed = issues.data.filter(issue => issue.status === "closed")
+    closed.forEach(issue => {
+
+        const div = document.createElement("div");
+        div.innerHTML = `
+       <div class="border-t-4 border-[#A855F7] rounded-sm p-3 bg-white shadow-sm space-y-3 min-h-60">
+            <div class="flex justify-between items-center">
+                <img src="./assets/./ClosedStatus.png" alt="">
+                <button
+                    class="btn bg-[#FEECEC] rounded-full h-5 font-medium text-[#EF4444] text-[12px]">${issue.priority}</button>
+            </div>
+
+            <h2 class="font-semibold text-[14px] text-[#1F2937]">${issue.title}</h2>
+            <p class="text-[#64748B] text-[12px] font-extralight">${issue.description}</p>
+            <hr class="text-gray-300">
+            <p class="text-[#64748B] text-[12px] font-[400]">#1by ${issue.author}</p>
+            <p class="text-[#64748B] text-[12px] font-[400]">${formatDate(issue.createdAt)}</p>
+        </div>
+    `;
+
+        container.appendChild(div);
+    })
+
+    issuesCount(closed.length);
+}
+
+
+
+
+
+// Issues count
+
+function issuesCount(count) {
+    document.getElementById("issues-counter").textContent = count;
+}
+
+loadIssues();
+//  activeButton("all-btn");
